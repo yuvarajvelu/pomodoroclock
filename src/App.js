730b2524b02213721ff672956481f926bc2dbea3
yuvarajvelu <42603669+yuvarajvelu@users.noreply.github.com>
2020-05-22 15:37:33 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.scss';
 import $ from 'jquery';
 
-let timer = "";
+let timerclock;
+//let t;
 let flag;
 let len = ""
 class App extends React.Component {
@@ -17,11 +18,12 @@ class App extends React.Component {
     this.handleDecrement = this.handleDecrement.bind(this);
     this.handleIncrement = this.handleIncrement.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleInc() {
     let l = this.state.breaklen;
     len = "";
-    if (l<=59) {
+    if (l<60) {
     this.setState(state=>({
       breaklen : state.breaklen+1
     }));
@@ -39,7 +41,7 @@ class App extends React.Component {
   handleIncrement() {
     len = "";
     let l = this.state.sessionlen;
-    if (l<=59) {
+    if (l<60) {
     this.setState(state=>({
       sessionlen : state.sessionlen+1
     }));
@@ -55,11 +57,13 @@ class App extends React.Component {
     }
   }
   handleReset() {
-    clearInterval(timer);
+    clearInterval(timerclock);
     this.setState({
       breaklen : 5,
       sessionlen : 25
     })
+    $(".display").css("color","black");
+    $(".display").css("border","2px solid black")
     $("#time-left").text("25:00");
     $(".change").prop("disabled",false);
     document.getElementById("timer-label").innerText = "Session";
@@ -68,61 +72,22 @@ class App extends React.Component {
     document.getElementById("beep").currentTime = 0;
     len = "";
   }
-  render() {
-    return(
-      <div id="pomodoro-clock">
-        <h1 className="heading">Pomodoro Clock</h1>
-        <Break breaklen={this.state.breaklen} handleInc={this.handleInc} handleDec={this.handleDec}/>
-        <Session sessionlen={this.state.sessionlen} handleIncrement={this.handleIncrement} handleDecrement={this.handleDecrement} />
-        <Timer sessionlen={this.state.sessionlen} breaklen={this.state.breaklen} handleReset={this.handleReset}/>
-      </div>
-    )
-  }
-}
-class Break extends React.Component {
-  render(){
-    return(
-    <div id="break">
-      <h2 className="title" id="break-label">Break Length</h2>
-      <button type="button" className="change" id="break-decrement" onClick={this.props.handleDec}><i className="fa fa-arrow-down"></i></button>
-      <h2 id="break-length">{this.props.breaklen}</h2>
-      <button type="button" className="change" id="break-increment" onClick={this.props.handleInc}><i className="fa fa-arrow-up"></i></button>
-    </div>  
-    )
-  }
-}
-class Session extends React.Component {
-  render(){
-    return(
-      <section id="session">
-        <h2 className="title" id="session-label">Session Length</h2>
-        <button type="button" className="change" id="session-decrement" onClick={this.props.handleDecrement}><i className="fa fa-arrow-down"></i></button>
-        <h2 id="session-length">{this.props.sessionlen}</h2>
-        <button type="button" className="change" id="session-increment" onClick={this.props.handleIncrement}><i className="fa fa-arrow-up"></i></button>
-      </section>
-    )
-  }
-}
-class Timer extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    
-    this.handleClick = this.handleClick.bind(this);
-  }
   handleClick() {
     let c = document.getElementById("play-pause").className;
     let aud = document.getElementById("beep");
+    let a = this.state.sessionlen;
+    let b = this.state.breaklen; 
+    
     if(c==="fa fa-play") {
       $(".change").prop("disabled",true);
       $("#play-pause").removeClass("fa-play").addClass("fa-pause");
       let seslen;
       if(len==="") {
-        seslen = this.props.sessionlen + "00";
+        seslen = this.state.sessionlen + "00";
       } else {
         seslen = len;
       }
-      timer = setInterval(()=>{
+      timerclock = setInterval(function timer(){
         if(seslen%100===0) {
           seslen = seslen - 41; 
         } else {
@@ -149,38 +114,62 @@ class Timer extends React.Component {
           aud.pause();
         }
         document.getElementById("time-left").innerText = seslen.slice(0,2)+":"+seslen.slice(2);
-        console.log(seslen)
         if(parseInt(seslen)===0) {
+          aud.currentTime = 0;
           aud.play();
           if(flag===1) {
             document.getElementById("timer-label").innerText = "Session";
-            seslen = this.props.sessionlen + "00";
+            if(seslen<10) {
+              seslen = '0'+a+'00'
+            }
+            else{
+              seslen = a + '00';
+            }
             flag = 0;
           }  else {
             document.getElementById("timer-label").innerText = "Break";
-            seslen = this.props.breaklen + '00';
+            if(seslen<10) {
+              seslen = '0'+b+'00'
+            }
+            else{
+              seslen = b + '00';
+            }
             flag = 1;
           }
         }
       },1000);
-    } else {
+      
+    } else if(c==="fa fa-pause") {
       $(".change").prop("disabled",false);
       $("#play-pause").removeClass("fa-pause").addClass("fa-play");
-      clearInterval(timer)
+      
+      clearInterval(timerclock);
     }
-  }
+  } 
   render() {
-    let min = this.props.sessionlen;
+    let min = this.state.sessionlen;
     let time = "";
     if(min>=10) {
       time =`${min}:00`;
     } else {
       time =`0${min}:00`;
     }
-   
-    
     return(
-      <div className="timer">
+      <div id="pomodoro-clock">
+        <h1 className="heading">Pomodoro Clock</h1>
+        <div id="break">
+          <h2 className="title" id="break-label">Break Length</h2>
+          <button type="button" className="change" id="break-decrement" onClick={this.handleDec}><i className="fa fa-arrow-down"></i></button>
+          <h2 id="break-length">{this.state.breaklen}</h2>
+          <button type="button" className="change" id="break-increment" onClick={this.handleInc}><i className="fa fa-arrow-up"></i></button>
+        </div>  
+        <section id="session">
+            <h2 className="title" id="session-label">Session Length</h2>
+            <button type="button" className="change" id="session-decrement" onClick={this.handleDecrement}><i className="fa fa-arrow-down"></i></button>
+            <h2 id="session-length">{this.state.sessionlen}</h2>
+            <button type="button" className="change" id="session-increment" onClick={this.handleIncrement}><i className="fa fa-arrow-up"></i></button>
+        </section>
+        <div className="timer">
         <div className="display">
           <h2 id="timer-label">Session</h2>
           <h1 id="time-left">{time}</h1>
@@ -188,11 +177,15 @@ class Timer extends React.Component {
         <audio id="beep" preload="auto" src="https://goo.gl/65cBl1" />
         <div id="options">
         <button id="start_stop"><i id="play-pause" className="fa fa-play" onClick={this.handleClick} ></i></button>
-        <button id="reset"><i className="fa fa-refresh" onClick={this.props.handleReset}></i></button>
+        <button id="reset"><i className="fa fa-refresh" onClick={this.handleReset}></i></button>
         </div>
+      </div>
       </div>
     )
   }
 }
+
+
+
 
 export default App;
